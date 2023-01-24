a83f6f3656f527353828bde63a0890e18e72fdbd
dgamarra <dgamarra@continental.edu.pe>
2023-01-24 09:51:45 -0500

    Implementación de la operación division

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from src.logica.OperacionesBasicas import OperacionesBasicas
 if __name__ == '__main__':
     operaciones = OperacionesBasicas()
     try:
-        sumando1 = float(input("Primer sumando: "))
-        sumando2 = float(input("Segundo sumando: "))
+        sumando1 = operaciones.ingresoParametro(input("Primer sumando: "))
+        sumando2 = operaciones.ingresoParametro(input("Segundo sumando: "))
         print(f"{sumando1:.2f} + {sumando2:.2f} = {operaciones.suma(sumando1,sumando2):.2f}")
     except ValueError:
         print("Error: Sumando1 y sumando2 deben ser int o float")
--- src/logica/OperacionesBasicas.py
+++ src/logica/OperacionesBasicas.py
@@ -5,4 +5,19 @@ class OperacionesBasicas:
         return sumando1 + sumando2
 
     def division(self, dividendo, divisor):
-        return dividendo / divisor
+        if not isinstance(dividendo, (int, float)) or not isinstance(divisor, (int, float)):
+            raise Exception("Oops! El Divideno y el divisor sumandos deben ser int o float.")
+
+        try:
+            return dividendo / divisor
+        except ZeroDivisionError:
+            print("El divisor no puede ser cero.")
+            exit(2)
+    def ingresoParametro(self, parametro):
+        if not isinstance(parametro, (int, float)):
+            try:
+                parametro=float(parametro)
+            except ValueError:
+                print("No se puede convertir a float")
+                exit(2)
+        return parametro

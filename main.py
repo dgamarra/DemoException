ee6142eef75df3edb0f5211a78e6c4dc33fb8828
dgamarra <dgamarra@continental.edu.pe>
2023-01-25 07:26:47 -0500

    Implemntado la suma y división

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ if __name__ == '__main__':
         sumando1 = operaciones.ingresoParametro(input("Primer sumando: "))
         sumando2 = operaciones.ingresoParametro(input("Segundo sumando: "))
         print(f"{sumando1:.2f} + {sumando2:.2f} = {operaciones.suma(sumando1,sumando2):.2f}")
-    except ValueError:
-        print("Error: Sumando1 y sumando2 deben ser int o float")
 
-    try:
-        dividendo = float(input("Primer dividendo: "))
-        divisor = float(input("Segundo divisor: "))
-        print(f"{dividendo:.2f} + {divisor:.2f} = {operaciones.division(dividendo,divisor):.2f}")
+        dividendo = operaciones.ingresoParametro(input("Dividendo: "))
+        divisor = operaciones.ingresoParametro(input("Divisor: "))
+        print(f"{dividendo:.2f} / {divisor:.2f} = {operaciones.division(dividendo,divisor):.2f}")
     except ValueError:
-        print("Error: Dividendo y divisor deben ser int o float")
+        print("Los parámetros deben ser int o float")
+    except ZeroDivisionError:
+        print("El divisor no puede ser cero.")
--- src/logica/OperacionesBasicas.py
+++ src/logica/OperacionesBasicas.py
@@ -1,23 +1,15 @@
 class OperacionesBasicas:
     def suma(self, sumando1, sumando2):
-        if not isinstance(sumando1, (int, float)) or not isinstance(sumando2, (int, float)):
-            raise Exception("Oops! Los sumandos deben ser int o float.")
         return sumando1 + sumando2
 
     def division(self, dividendo, divisor):
-        if not isinstance(dividendo, (int, float)) or not isinstance(divisor, (int, float)):
-            raise Exception("Oops! El Divideno y el divisor sumandos deben ser int o float.")
+        return dividendo / divisor
 
-        try:
-            return dividendo / divisor
-        except ZeroDivisionError:
-            print("El divisor no puede ser cero.")
-            exit(2)
+
     def ingresoParametro(self, parametro):
         if not isinstance(parametro, (int, float)):
             try:
                 parametro=float(parametro)
             except ValueError:
-                print("No se puede convertir a float")
-                exit(2)
+                 raise ValueError
         return parametro
